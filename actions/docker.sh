9e3701aa287b26803d95f5e43a73db648441cc06
xaazias <flamesuspect@gmail.com>
2021-10-30 19:16:20 +0300

    Changed docker.sh script.

--- actions/docker.sh
+++ actions/docker.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if docker build -t $APP:$GIT_TAG_NAME . ; then
-  echo "DOCKER_STATUS='Build $APP:$GIT_TAG_NAME completed.'" >> $GITHUB_ENV
+  echo "DOCKER_STATUS=Build $APP:$GIT_TAG_NAME completed." >> $GITHUB_ENV
 else 
-  echo "DOCKER_STATUS='Build was not completed due to an error.'" >> $GITHUB_ENV
+  echo "DOCKER_STATUS=Build was not completed due to an error." >> $GITHUB_ENV
 fi
